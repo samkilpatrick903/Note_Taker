0726f558a260f3e22bdf2857905fda17173b2efa
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-12-04 12:02:48 -0600

    updated server

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -1,14 +1,12 @@
-const router = require('express').Router(); //import router (built into express)
+const router = require("express").Router();
 const path = require("path");
 
-//HTML ROUTE: get * should return the index.html file
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-//HTML ROUTE: get /notes should return the notes.html file
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
 module.exports = router;
